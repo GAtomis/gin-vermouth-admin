29f8420737fdea588b7953c351ed952902ef1b65
GAtomis <850680822@qq.com>
2024-06-14 11:40:39 +0800

    update ： 权限

--- src/view/superAdmin/authority/authority.vue
+++ src/view/superAdmin/authority/authority.vue
@@ -3,9 +3,17 @@
     <header>
       <a-button type="primary">新增</a-button>
     </header>
-
-
-
-
+    <main>
+      <a-table :columns="columns" :data-source="dataSource" bordered>
+      </a-table>
+    </main>
   </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const dataSource = ref()
+const columns = {}
+
+
+
+</script>
