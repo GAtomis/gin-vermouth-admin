bb90b30813c95481d41791608617821432e48d97
GAtomis <850680822@qq.com>
2024-07-03 23:14:57 +0800

    update；role

--- src/api/account/menu.ts
+++ src/api/account/menu.ts
@@ -2,81 +2,25 @@
  * @Description: 菜单
  * @Author: Gavin
  * @Date: 2022-08-03 11:15:05
- * @LastEditTime: 2024-05-31 09:56:31
+ * @LastEditTime: 2024-07-03 23:13:37
  * @LastEditors: GAtomis 850680822@qq.com
  */
-import type { Primarykey, PageInfo, PageStruct } from '@/model/common'
-import type { Permission } from '@/model/account'
-import http from '@/utils/request-api'
+import type { Primarykey, PageInfo, PageStruct } from '@/model/common';
+import type { Permission } from '@/model/account';
+import http from '@/utils/request-api';
 export function getListByRoleId<R = Permission[]>() {
-  return http.request<R>(
-    {
-      url: '/permission/getListByRoleId',
-      method: 'GET',
-    },
-  )
+  return http.request<R>({
+    url: '/permission/getListByRoleId',
+    method: 'GET',
+  });
 }
 /**
  * @description: 请求列表
  * @return {*}
  */
-export function getMenusByToken(){
-  return http.request<any,Result<{menus:Menu[]}>>(
-    {
-      url: '/menu/getMenu',
-      method: 'POST',
-    },
-  )  
+export function getMenusByToken() {
+  return http.request<any, Result<{ menus: Menu[] }>>({
+    url: '/menu/getBaseMenuTree',
+    method: 'POST',
+  });
 }
-
-
-
-
-
-// export function getList<Q = PageInfo, R = PageStruct<Permission>>(params: Q) {
-//   return http.request<R>(
-//     {
-//       url: '/permission/list',
-//       method: 'GET',
-//       params,
-//     },
-//   )
-// }
-// export function createItem<Q = Permission, R = string>(data: Q) {
-//   return http.request<R>(
-//     {
-//       url: '/permission/permission',
-//       method: 'POST',
-//       data,
-//     }
-//   )
-// }
-// export function updateItem<Q = Permission, R = string>(data: Q) {
-//   return http.request<R>(
-//     {
-//       url: '/permission/permission',
-//       method: 'PUT',
-//       data,
-//     }
-//   )
-// }
-
-// export function delItem<Q = Permission, R = string>(data: Q) {
-//   return http.request<R>(
-//     {
-//       url: '/permission/permission',
-//       method: 'DELETE',
-//       data,
-//     }
-//   )
-// }
-
-// export function getItem<Q = Primarykey, R = Permission>(params: Q) {
-//   return http.request<R>(
-//     {
-//       url: '/permission/permission',
-//       method: 'GET',
-//       params,
-//     }
-//   )
-// }
--- src/view/superAdmin/authority/authority.vue
+++ src/view/superAdmin/authority/authority.vue
@@ -4,7 +4,25 @@
       <a-button type="primary">新增</a-button>
     </header>
     <main>
-      <a-table :dataSource="dataSource" :columns="columns" />
+      <a-table :dataSource="dataSource" :columns="columns">
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'action'">
+            <a class="alink" @click="() => (open = true)">设置权限</a>
+            <a class="alink">新增子角色</a>
+            <a class="alink">拷贝</a>
+            <a class="alink">编辑</a>
+            <a class="alink">删除</a>
+          </template>
+        </template>
+      </a-table>
+
+      <a-modal v-model:open="open" title="设置权限">
+        <a-tabs v-model:activeKey="activeKey" :tab-position="'left'" animated>
+          <a-tab-pane key="1" tab="Tab 1">Content of Tab 1</a-tab-pane>
+          <a-tab-pane key="2" tab="Tab 2">Content of Tab 2</a-tab-pane>
+          <a-tab-pane key="3" tab="Tab 3">Content of Tab 3</a-tab-pane>
+        </a-tabs>
+      </a-modal>
     </main>
   </div>
 </template>
@@ -24,11 +42,13 @@
       key: 'authorityName',
     },
     {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
+      title: '控制',
+      key: 'action',
     },
   ];
+  const open = ref(false);
+  const activeKey = ref(false);
+
   const pager = ref<Pager>({
     page: 1,
     pageSize: 10,
@@ -42,3 +62,8 @@
   };
   getList();
 </script>
+<style scoped lang="scss">
+  .alink {
+    margin-right: 20px;
+  }
+</style>
